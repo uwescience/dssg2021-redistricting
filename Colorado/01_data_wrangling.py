b3cb95f7ea0fb56225e142623d06618503a47c57
Katherine Chang <kachang@uw.edu>
2021-07-23 15:41:35 -0700

    Added CO election data

---
+++ Colorado/01_data_wrangling.py
@@ -0,0 +1,10 @@
+
+"""
+01. Data Wrangling
+
+Description: This file pulls election data from Colorado's Secretary of State Office and 
+merges it into a single shapefile to use for analysis. 
+
+"""
+
+
