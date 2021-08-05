80d44468cfbb41b9ef4289e9be63f628f0357c28
Katherine Chang <kachang@uw.edu>
2021-08-05 02:50:03 -0700

    Continued CO data cleaning + Started CO EDA

--- Colorado/01_CO_data_wrangling.py
+++ Colorado/01_CO_data_wrangling.py
@@ -58,7 +58,7 @@ def election_df(precinct_df, election_name):
                                            & (precinct_df_trim["Party"] == "Republican Party")]  
     
     return election_name_d, election_name_r
-
+"""
 def election_build(election_build_df, election_build_var):
    
     eb_count = len(election_build_df)
@@ -71,7 +71,8 @@ def election_build(election_build_df, election_build_var):
         elections_all = elections_all.join(election_build.set_index('PRECID'), on='PRECID')
             
     return elections_all
-            
+"""     
+
 #--- 2020
 
 precincts_2020 = pd.read_excel("Data/Elections/2020_general_precincts.xlsx",
@@ -103,13 +104,15 @@ election_build_var_2020 = ["PRE20D",
                            ]
 
 df_elections = precincts_2020_pres_d["Precinct"].to_frame()
-df_elections = elections_all.rename({'Precinct':'PRECID'}, axis=1)
+df_elections = df_elections.rename({'Precinct':'PRECID'}, axis=1)
+df_elections = df_elections.astype("string")
 
 for n in range(6):    
     election_build = election_build_df_2020[n].loc[:, ["Precinct", "Candidate Votes"]]
     election_build.columns = ["PRECID", str(election_build_var_2020[n])]
-    election_build.applymap(int)
+    election_build = election_build.astype("string")
     df_elections = df_elections.join(election_build.set_index('PRECID'), on='PRECID')
+    
 
 #--- 2018
 
@@ -129,7 +132,7 @@ election_build_var_2018 = ["HOU18D",
 for n in range(2):    
     election_build = election_build_df_2018[n].loc[:, ["Precinct", "Candidate Votes"]]
     election_build.columns = ["PRECID", str(election_build_var_2018[n])]
-    election_build.applymap(int)
+    election_build = election_build.astype("string")
     df_elections = df_elections.join(election_build.set_index('PRECID'), on='PRECID')
 
 #--- 2016
@@ -165,7 +168,7 @@ election_build_var_2016 = ["PRE16D",
 for n in range(6):    
     election_build = election_build_df_2016[n].loc[:, ["Precinct", "Candidate Votes"]]
     election_build.columns = ["PRECID", str(election_build_var_2016[n])]
-    election_build.applymap(int)
+    election_build = election_build.astype("string")
     df_elections = df_elections.join(election_build.set_index('PRECID'), on='PRECID')
 
 #--- 2014
@@ -195,7 +198,7 @@ election_build_var_2014 = ["SEN14D",
 for n in range(4):    
     election_build = election_build_df_2014[n].loc[:, ["Precinct", "Candidate Votes"]]
     election_build.columns = ["PRECID", str(election_build_var_2014[n])]
-    election_build.applymap(int)
+    election_build = election_build.astype("string")
     df_elections = df_elections.join(election_build.set_index('PRECID'), on='PRECID')
 
 #--- 2012
@@ -203,7 +206,163 @@ for n in range(4):
 precincts_2012 = pd.read_excel("Data/Elections/2012_general_precincts.xlsx",
                                engine="openpyxl")
 
+
+precincts_2012_pres_d = election_df(precincts_2012, "President/Vice President")[0]
+precincts_2012_pres_r = election_df(precincts_2012, "President/Vice President")[1]
+
+precincts_2012_house_d = election_df(precincts_2012, "United States Representative")[0]
+precincts_2012_house_r = election_df(precincts_2012, "United States Representative")[1]
+
+
+election_build_df_2012 = [precincts_2012_pres_d,
+                          precincts_2012_pres_r,
+                          precincts_2012_house_d,
+                          precincts_2012_house_r
+                          ]
+
+election_build_var_2012 = ["PRE12D",
+                           "PRE12R",
+                           "HOU12D",
+                           "HOU12R"
+                           ]
+for n in range(4):    
+    election_build = election_build_df_2012[n].loc[:, ["Precinct", "Candidate Votes"]]
+    election_build.columns = ["PRECID", str(election_build_var_2012[n])]
+    election_build = election_build.astype("string")
+    df_elections = df_elections.join(election_build.set_index('PRECID'), on='PRECID')
+
+elections_2010_2020 = df_elections
+
+### Handcleaning SOS files from 2004-2010 due to differences in variable names and format for each year 
+
 #--- 2010
 
 precincts_2010 = pd.read_excel("Data/Elections/2010_general_precincts.xlsx",
                                engine="openpyxl")
+
+precincts_2010_trim = precincts_2010[~precincts_2010["Precinct"].astype(str).str.startswith("P")] #Removing provisional
+
+precincts_2010_trim = precincts_2010_trim.groupby(['Precinct', 'Office/Question', 'Party'], as_index=False).sum()
+precincts_2010_trim = precincts_2010_trim[(precincts_2010_trim["Party"] == "DEM") 
+                                          | (precincts_2010_trim["Party"] == "REP")]
+
+precincts_2010_senate_d = precincts_2010_trim[(precincts_2010_trim["Office/Question"] == "UNITED STATES SENATOR")
+                                              & (precincts_2010_trim["Party"] == "DEM")]  
+precincts_2010_senate_r = precincts_2010_trim[(precincts_2010_trim["Office/Question"] == "UNITED STATES SENATOR")
+                                              & (precincts_2010_trim["Party"] == "REP")]  
+
+precincts_2010_house_d = precincts_2010_trim.loc[(precincts_2010_trim["Office/Question"].str.startswith("REPRESENTATIVE TO THE 112th UNITED STATES CONGRESS"))
+                                                 & (precincts_2010_trim["Party"] == "DEM")]  
+precincts_2010_house_r = precincts_2010_trim.loc[(precincts_2010_trim["Office/Question"].str.startswith("REPRESENTATIVE TO THE 112th UNITED STATES CONGRESS"))
+                                           & (precincts_2010_trim["Party"] == "REP")]  
+
+election_build_df_2010 = [precincts_2010_senate_d,
+                          precincts_2010_senate_r,
+                          precincts_2010_house_d,
+                          precincts_2010_house_r
+                          ]
+
+election_build_var_2010 = ["SEN10D",
+                           "SEN10R",
+                           "HOU10D",
+                           "HOU10R"
+                           ]
+
+elections_2004_2010 = precincts_2010_senate_d["Precinct"].to_frame()
+elections_2004_2010 = elections_2004_2010.rename({'Precinct':'PRECID'}, axis=1)
+elections_2004_2010 = elections_2004_2010.astype("string")
+elections_2004_2010 = elections_2004_2010.drop_duplicates(subset=['PRECID'])
+
+for n in range(4):    
+    election_build = election_build_df_2010[n].loc[:, ["Precinct", "Votes"]]
+    election_build.columns = ["PRECID", str(election_build_var_2010[n])]
+    election_build = election_build.astype("string")
+    elections_2004_2010 = elections_2004_2010.join(election_build.set_index('PRECID'), on='PRECID')
+
+"""
+precincts_2010_house_d[['Office/Question', 'CONDIS_2010']] = precincts_2010_house_d['Office/Question'].str.split(' - ', expand=True)
+
+condist_2010 = precincts_2010_house_d.loc[:, ["Precinct", "CONDIS_2010"]]
+condist_2010.columns = ["PRECID", "CONDIS_2010"]
+condist_2010 = condist_2010.astype("string")
+elections_2004_2010 = elections_2004_2010.join(condist_2010.set_index('PRECID'), on='PRECID')
+"""
+#--- 2008 
+
+precincts_2008 = pd.read_excel("Data/Elections/2008_general_precincts.xlsx",
+                               engine="openpyxl")
+
+precincts_2008_trim = precincts_2008[precincts_2008["Precinct"].astype(str).str.startswith("P")] #Removing provisional
+precincts_2008_trim = precincts_2008.groupby(['Precinct', 'Office/Ballot Issue', 'Party'], as_index=False).sum()
+precincts_2008_trim = precincts_2008_trim.astype("string")
+
+precincts_2008_trim["Party"] = precincts_2008_trim["Party"].str.strip()
+precincts_2008_trim["Office/Ballot Issue"] = precincts_2008_trim["Office/Ballot Issue"].str.strip()
+precincts_2008_trim["Precinct"] = precincts_2008_trim["Precinct"].str.strip()
+
+precincts_2008_trim = precincts_2008_trim[(precincts_2008_trim["Party"] == "Democrat") 
+                                          | (precincts_2008_trim["Party"] == "Republican")]
+
+precincts_2008_pres_d = precincts_2008_trim[(precincts_2008_trim["Office/Ballot Issue"] == "PRESIDENTIAL ELECTORS/ PRESIDENTIAL ELECTORS (VICE)")
+                                            & (precincts_2008_trim["Party"] == "Democrat")]  
+precincts_2008_pres_r = precincts_2008_trim[(precincts_2008_trim["Office/Ballot Issue"] == "PRESIDENTIAL ELECTORS/ PRESIDENTIAL ELECTORS (VICE)")
+                                            & (precincts_2008_trim["Party"] == "Republican")]  
+
+precincts_2008_senate_d = precincts_2008_trim[(precincts_2008_trim["Office/Ballot Issue"] == "UNITED STATES SENATOR")
+                                              & (precincts_2008_trim["Party"] == "Democrat")]  
+precincts_2008_senate_r = precincts_2008_trim[(precincts_2008_trim["Office/Ballot Issue"] == "UNITED STATES SENATOR")
+                                              & (precincts_2008_trim["Party"] == "Republican")]  
+
+
+precincts_2008_house_d = precincts_2008_trim.loc[(precincts_2008_trim["Office/Ballot Issue"].str.startswith("REPRESENTATIVE TO THE 111th UNITED STATES CONGRESS"))
+                                                 & (precincts_2008_trim["Party"] == "Democrat")]  
+
+precincts_2008_house_r = precincts_2008_trim.loc[(precincts_2008_trim["Office/Ballot Issue"].str.startswith("REPRESENTATIVE TO THE 111th UNITED STATES CONGRESS"))
+                                           & (precincts_2008_trim["Party"] == "Republican")]  
+
+
+election_build_df_2008 = [precincts_2008_pres_d,
+                          precincts_2008_pres_r,
+                          precincts_2008_senate_d,
+                          precincts_2008_senate_r,
+                          precincts_2008_house_d,
+                          precincts_2008_house_r
+                          ]
+
+election_build_var_2008 = ["PRES08D",
+                           "PRES08R",
+                           "SEN08D",
+                           "SEN08R",
+                           "HOU08D",
+                           "HOU08R"
+                           ]
+
+for n in range(6):    
+    election_build = election_build_df_2008[n].loc[:, ["Precinct", "Votes"]]
+    election_build.columns = ["PRECID", str(election_build_var_2008[n])]
+    election_build = election_build.astype("string")
+    elections_2004_2010 = elections_2004_2010.join(election_build.set_index('PRECID'), on='PRECID')
+
+"""
+#Delim congressional district Number 
+#precincts_2008_house_d[['Office/Ballot Issue', 'CONDIS_2008']] = precincts_2008_house_d['Office/Ballot Issue'].str.split(' - ', expand=True)
+
+condist_2008 = precincts_2008_house_d.loc[:, ["Precinct", "CONDIS_2008"]]
+condist_2008.columns = ["PRECID", "CONDIS_2008"]
+condist_2008 = condist_2008.astype("string")
+elections_2004_2010 = elections_2004_2010.join(condist_2008.set_index('PRECID'), on='PRECID')
+"""
+
+#--- 2006
+
+precincts_2006 = pd.read_excel("Data/Elections/2006_general_precincts.xlsx",
+                               engine="openpyxl")
+
+#--- 2004
+
+precincts_2004 = pd.read_excel("Data/Elections/2004_general_precincts.xlsx",
+                               engine="openpyxl")
+
+
+
+
--- Colorado/02_CO_metrics_modeling.py
+++ Colorado/02_CO_metrics_modeling.py
@@ -5,9 +5,6 @@
 Description: This file is dedicated to state-specific metrics development and appropriate
 tests to refine modeling decisions.
 
-- (Top Level Analysis) Partisan Competitiveness
-- (Nuanced) Gender/Racial Minority Reprsentation
-
 https://redistricting.colorado.gov/content/redistricting-laws Congressional Language (Section 44.3):
 “Thereafter, the commission shall, to the extent possible, maximize the number of politically competitive districts…
 ‘competitive’ means having a reasonable potential for the party affiliation of the district’s representative to change
@@ -17,3 +14,53 @@ a proposed district’s political party registration data,
 and evidence-based analyses of proposed districts.”
 
 """
+
+import os
+import sys
+import pandas as pd
+import geopandas as gpd
+from gerrychain import Graph
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+sys.path.insert(0, os.getenv("REDISTRICTING_HOME"))
+import utility_functions as uf
+
+try:
+    os.chdir(os.path.join(os.getenv("REDISTRICTING_HOME", default=""),
+                          "Colorado"))
+except OSError:
+    os.mkdir(os.path.join(os.getenv("REDISTRICTING_HOME", default=""),
+                          "Colorado"))
+    os.chdir(os.path.join(os.getenv("REDISTRICTING_HOME", default=""),
+                          "Colorado"))
+       
+#--- IMPORT DIFFERENT DATA TO EXPLORE
+
+#Metric Geometry and Gerrymandering Group
+graph_mggg = Graph.from_json("Data/co_precincts.json")
+df_mggg = gpd.read_file("Data/co_precincts.shp")
+
+uf.plot_district_map(df_mggg, df_mggg['CD116FP'].to_dict(), "Current Congressional District Map")
+
+nx.draw(graph_mggg,pos = {node:(graph_mggg.nodes[node]["C_X"],graph_mggg.nodes[node]["C_Y"]) 
+                     for node in graph_mggg.nodes()},node_color=[graph_mggg.nodes[node]["CD116FP"] 
+                                                            for node in graph_mggg.nodes()],node_size=10,cmap='tab20')
+#Redistricting Data Hub
+df_rdh_2016 = gpd.read_file("Data/RDH/co_vest_16/co_vest_16.shp")                      
+df_rdh_2018 = gpd.read_file("Data/RDH/co_vest_18/co_vest_18.shp")
+df_rdh_2020 = gpd.read_file("Data/RDH/co_vest_20/co_vest_20.shp")
+
+#Secretary of State Election Data (2004-2020)
+
+#--- Political Competitiveness
+
+"""
+- Different vote bands, 5%, 10%, and 15%
+- Win margin between the two major political parties
+
+"""
+
+
+                                                                 
